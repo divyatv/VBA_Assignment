a9f3049e2910e529a6c10d0162154160132c9f25
divya tv <divya.tv@gmail.com>
2019-06-04 10:22:31 -0400

    dot vb file with code

--- VB_Code_Assignment_Easy.vb
+++ VB_Code_Assignment_Easy.vb
@@ -1,26 +1,27 @@
-' Homework easy - get the ticket symbol and total stock volume for all the etf
-' Sample code structure or logic
+'Homework easy - get the ticker symbol and total stock volume for all the etf - add challenge to go over all the worksheets of the file.
+'Sample code structure or logic
 'go over all the sheets
 'go over all the rows in each sheets using last row
-'set a ticker symbol - to begin with the first row
-'set the total volume - to start with zero
-'check the cell with next cell and if they are not equal - use the <> sign
-' if they are same stock then add the total
-'If they are not same stock then reset total to fist row of new ticker, reset summry table row to next one, reset ticker to next stock.
-' Do the steps for all the worksheets.
-
+'Set a ticker symbol - to begin with the first row
+'Set the total volume - to start with zero
+'Check the cell with next cell and if they are not equal - use the <> sign
+'If they are same stock then add the total
+'If they are not same stock then reset total to fist row of new ticker, reset summary table row to next one, reset ticker to next stock.
+'Do the steps for all the worksheets.
+'------------------------------------------------------------------------------------------
 Sub sumtotal()
 
 'Initialize a variable to get the count of active worksheets
 'Dim ws_count As Integer
 
-'initialize a counter to go over all the worksheets.
+'Initialize a counter to go over all the worksheets.
 'Dim ws As Integer
 
-' Get the count of all the worksheets in the excel file.
+'Get the count of all the worksheets in the excel file.
 'ws_count = ActiveWorkbook.Worksheets.Count
 'MsgBox ("active sheet in this file=" & ws_count)
 
+'Initializing the variable to get the worksheets.
 Dim wk As Worksheet
 
 'Loop through each work sheet
@@ -39,7 +40,7 @@ wk.Activate
     'MsgBox ("totals=" & totals)
     
     Dim ticker As String
-    'start ticket with the first symbol
+    'start ticker with the first symbol
     ticker = Cells(2, 1).Value
     
     ' set and get the last row count to loop
@@ -50,7 +51,7 @@ wk.Activate
     Dim summarytablerow As Integer
     summarytablerow = 2
 
-            'for loop to lopp through all the rows until empty
+            'for loop to loop through all the rows until empty - starting loop from row2 to manage the headers.
                 For i = 2 To lastrow
             
                                     
@@ -60,7 +61,7 @@ wk.Activate
                                 Cells(summarytablerow, 10).Value = ticker
                                 Cells(summarytablerow, 11).Value = totals
                                 
-                                'reset the total volume to the first total volume of new stock.
+                                'Reset the total volume to the first total volume of new stock.
                                 totals = Cells(i + 1, 7).Value
                                 'Set the summary table to write to next row making sure summary table doesnt get overwritten.
                                  summarytablerow = summarytablerow + 1
@@ -81,7 +82,7 @@ wk.Activate
                           Next i
 
     'Next ws
-     ' Insert your code here.
+  
             ' This line displays the worksheet name in a message box.
             MsgBox wk.Name
   Next wk
